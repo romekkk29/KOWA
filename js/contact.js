e0f1be33e61aa6551d801227a5af9592249bbace
romekkk29 <romegomez29@gmail.com>
2021-12-03 11:52:32 -0300

    contact

---
+++ js/contact.js
@@ -0,0 +1,11 @@
+function stickyScroll(){
+    
+    window.addEventListener("scroll", function(){
+   
+     
+        let sticky= document.querySelector(".sticky")
+        sticky.classList.add("stickyScroll")}
+       
+    )
+}
+stickyScroll();
