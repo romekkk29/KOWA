34b77d56fff91a618b93014b94d9612ed1752e93
romekkk29 <romegomez29@gmail.com>
2021-12-05 20:52:14 -0300

    empezando con mobile

--- js/main.js
+++ js/main.js
@@ -1,10 +1,56 @@
+let ancho2= window.innerWidth || document.body.clientWidth;
+let ancho= ancho2/2;
+console.log(ancho);
+window.addEventListener("resize",function(){
+    location.reload();
+    ancho= screen.width;
+    entraIf();
 
+})
+function entraIf(){
+    if(ancho<500){
+        let aqui=document.querySelector(".aqui");
+        let imaFle=document.querySelector("#imaFle");
+        imaFle.style.width="40%";
+        imaFle.style.height="40%";
+        imaFle.style.position="absolute";
+        imaFle.style.left="29%";
+        imaFle.style.top="29%";
+        aqui.classList.remove("flecha");
+        aqui.classList.add("flechita");
+        let aqui2=document.querySelector(".aqui2");
+        aqui2.classList.remove("carrusel");
+        aqui2.classList.add("carrusel2");
+        carrusel2();
+      
+    }
+    else{
+        let imaFle=document.querySelector("#imaFle");
+        imaFle.style.width="100%";
+        imaFle.style.height="100%";
+        imaFle.style.position="inherit";
+        let aqui=document.querySelector(".aqui");
+        aqui.classList.remove("flechita");
+        aqui.classList.add("flecha");
+        let aqui2=document.querySelector(".aqui2");
+        aqui2.classList.remove("carrusel2");
+        aqui2.classList.add("carrusel");
+        carrusel3();
+      
+    }
+}
+entraIf();
 
 function stickyScroll(){
     
     window.addEventListener("scroll", function(){
         let sticky= document.querySelector(".sticky"); 
        sticky.classList.add("stickyScroll");
+       var y = window.scrollY;
+
+       if(y==0){
+        sticky.classList.remove("stickyScroll");
+       }
     }
        
     )
@@ -12,15 +58,16 @@ function stickyScroll(){
 stickyScroll();
 
 
-    let carrusel=document.querySelector("#carrusel");
+    
     let contador=0;
     let img=document.querySelector(".nasty");
     let gato=document.querySelector(".gato");
     let titulo=document.querySelector(".titulo");
     let buller=document.querySelector(".buller");
- 
     let identify=document.querySelector(".identify");
-    let proyectLink=document.querySelector("#proyectLink")
+    let proyectLink=document.querySelector("#proyectLink");
+function carrusel3(){
+    let carrusel=document.querySelector(".carrusel");
     carrusel.addEventListener("click", function(e){
         e.preventDefault();
         console.log(contador);
@@ -34,7 +81,7 @@ stickyScroll();
             buller.style.transform="translate(-70%)";
             titulo.innerHTML="Mandatos";
             identify.innerHTML="Campaña - Eva Miller";
-            proyectLink.innerHTML="<a href=#><p class=proyect>See proyect</p> ";
+            proyectLink.innerHTML="<a id=subra3  href=#><p class=proyect>See proyect</p><div class=subrayado2></div></a>";
             contador= contador+1;
             console.log(contador);
             break;
@@ -44,7 +91,7 @@ stickyScroll();
             buller.style.transform="translate(-110%)";
             titulo.innerHTML="Buller";
             identify.innerHTML="Web Site";
-            proyectLink.innerHTML="<a href=#><p class=proyect>See proyect</p> ";
+            proyectLink.innerHTML="<a id=subra3  href=#><p class=proyect>See proyect</p><div class=subrayado2></div></a>";
             contador= contador+1;
             gato.style.zIndex="14";
             buller.style.zIndex="12";
@@ -58,7 +105,7 @@ stickyScroll();
             img.style.transform="translate(-223%)";
             buller.style.transform="translate(-50%)";
             titulo.innerHTML="Nasty People";
-            proyectLink.innerHTML="<a href=#><p class=proyect>See proyect</p> ";
+            proyectLink.innerHTML="<a id=subra3  href=#><p class=proyect>See proyect</p><div class=subrayado2></div></a>";
             gato.style.left="300%";
             gato.style.transform="translate(-250%)";
             gato.style.zIndex="1";
@@ -78,7 +125,7 @@ stickyScroll();
                 buller.style.transform="translate(-270%)";
                 titulo.innerHTML="Mandatos";
                 identify.innerHTML="Campaña - Eva Miller";
-                proyectLink.innerHTML="<a href=#><p class=proyect>See proyect</p> ";
+                proyectLink.innerHTML="<a id=subra3  href=#><p class=proyect>See proyect</p><div class=subrayado2></div></a>";
                 contador= contador+1;
                 console.log(contador);
                 break; 
@@ -88,7 +135,7 @@ stickyScroll();
             buller.style.transform="translate(-310%)";
             titulo.innerHTML="Buller";
             identify.innerHTML="Web Site";
-            proyectLink.innerHTML="<a href=#><p class=proyect>See proyect</p> ";
+            proyectLink.innerHTML="<a id=subra3  href=#><p class=proyect>See proyect</p><div class=subrayado2></div></a>";
             contador= contador+1;
             gato.style.zIndex="14";
             buller.style.zIndex="12";
@@ -106,4 +153,94 @@ stickyScroll();
                 
 
         }
-    })
+    })}
+    function carrusel2(){
+        let carrusel2=document.querySelector(".carrusel2");
+        carrusel2.addEventListener("click", function(e){
+            e.preventDefault();
+            console.log(contador);
+            switch(contador){
+                case 0:   
+                gato.style.visibility="visible";
+                
+                img.style.transform="translate(-100%)";
+                gato.style.transform="translate(-55%)";
+                buller.style.left="translate(100%)";
+                buller.style.transform="translate(-70%)";
+                titulo.innerHTML="Mandatos";
+                identify.innerHTML="Campaña - Eva Miller";
+                proyectLink.innerHTML="<a id=subra3  href=#><p class=proyect>See proyect</p><div class=subrayado2></div></a>";
+                contador= contador+1;
+                console.log(contador);
+                break;
+                case 1: 
+                buller.style.visibility="visible";
+                gato.style.transform="translate(-160%)";
+                buller.style.transform="translate(-110%)";
+                titulo.innerHTML="Buller";
+                identify.innerHTML="Web Site";
+                proyectLink.innerHTML="<a id=subra3  href=#><p class=proyect>See proyect</p><div class=subrayado2></div></a>";
+                contador= contador+1;
+                gato.style.zIndex="14";
+                buller.style.zIndex="12";
+                img.style.left="200%";
+                img.style.transform="translate(-100%)";
+                
+                console.log(contador);
+                break;
+                case 2:
+                img.style.visibility="visible";
+                img.style.transform="translate(-200%)";
+                titulo.innerHTML="Nasty People";
+                proyectLink.innerHTML="<a id=subra3  href=#><p class=proyect>See proyect</p><div class=subrayado2></div></a>";
+                gato.style.left="300%";
+                gato.style.transform="translate(-200%)";
+                gato.style.zIndex="1";
+                buller.style.zIndex="45";
+                buller.style.transform="translate(-210%)";
+                identify.innerHTML="Identity for vorterix";
+    
+                contador++;
+                console.log(contador);
+                break; 
+                case 3:
+                    gato.style.visibility="visible";
+                    img.style.transform="translate(-300%)";
+                    gato.style.transform="translate(-300%)";
+                    buller.style.zIndex="0";
+                    buller.style.left="300%";
+                    buller.style.transform="translate(-200%)";
+                    titulo.innerHTML="Mandatos";
+                    identify.innerHTML="Campaña - Eva Miller";
+                    proyectLink.innerHTML="<a id=subra3  href=#><p class=proyect>See proyect</p><div class=subrayado2></div></a>";
+                    contador= contador+1;
+                    console.log(contador);
+                    break; 
+                case 4: 
+                buller.style.visibility="visible";
+                gato.style.transform="translate(-400%)";
+                buller.style.transform="translate(-299%)";
+                titulo.innerHTML="Buller";
+                identify.innerHTML="Web Site";
+                proyectLink.innerHTML="<a id=subra3  href=#><p class=proyect>See proyect</p><div class=subrayado2></div></a>";
+                contador= contador+1;
+                gato.style.zIndex="14";
+                buller.style.zIndex="12";
+                img.style.zIndex="-2";
+                img.style.left="400%";
+                img.style.transform="translate(-393%)";
+                
+                console.log(contador);
+                break;
+                case 5:
+                 
+                location.reload();
+                    break; 
+    
+                    
+    
+            }
+        })
+        
+    }
+
--- js/playroom.js
+++ js/playroom.js
@@ -3,6 +3,10 @@ function stickyScroll(){
     window.addEventListener("scroll", function(){
         let sticky= document.querySelector(".sticky"); 
        sticky.classList.add("stickyScroll");
+       var y = window.scrollY;
+       if(y==0){
+        sticky.classList.remove("stickyScroll");
+       }
     }
        
     )
