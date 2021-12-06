938711e61c2e3841ca964fd2f2853fc4f31a014b
romekkk29 <romegomez29@gmail.com>
2021-12-06 20:02:14 -0300

    va queriendo

--- js/contact.js
+++ js/contact.js
@@ -1,8 +1,10 @@
+/* let body=document.querySelector("body");
 function stickyScroll(){
-    window.addEventListener("scroll", function(){   
+    body.addEventListener("scroll", function(){   
         let sticky= document.querySelector(".sticky");
         sticky.classList.add("stickyScroll");
-        var y = window.scrollY;
+        var y = body.scrollTop;
+        console.log(y);
         if(y==0){
          sticky.classList.remove("stickyScroll");
         }
@@ -11,4 +13,33 @@ function stickyScroll(){
        
     )
 }
-stickyScroll();
+stickyScroll(); */
+
+let body= document.querySelector("body");
+
+console.log(body)
+    body.addEventListener("scroll", function(){   
+        let sticky= document.querySelector(".sticky");
+        sticky.classList.add("stickyScroll");
+        let y = body.scrollTop;
+        console.log(y)
+   
+        if(y==0){
+         sticky.classList.remove("stickyScroll");
+        }
+        
+    }
+       
+    )
+    let menu=document.querySelector("#menu");
+    let xBar=document.querySelector("#xBar");
+    
+    let menuBar=document.querySelector("#menuBar");
+    menu.addEventListener("click",function(e){
+        e.preventDefault();
+     
+       menuBar.style.transform="translate(0%)"
+    })    
+    xBar.addEventListener("click",function(){
+    menuBar.style.transform="translate(100%)"}
+    )
