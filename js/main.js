69fd9f2af1c14df0669e205aa8cf038db8145d00
romekkk29 <romegomez29@gmail.com>
2021-12-08 11:55:58 -0300

    daleee

--- js/main.js
+++ js/main.js
@@ -60,7 +60,7 @@ img11.addEventListener("click",function(){
 function agarraNus(){
     let agarraNusty=document.querySelector("#agarraNusty");
     agarraNusty.addEventListener("click",function(){
-        const plantilla=`<div id="fixea">  <img loading="lazy" src="img/nasty1.jpg" alt=""><img loading="lazy" src="img/nasty2.jpg" alt=""><img loading="lazy" src="img/nasty3.jpg" alt=""><img  loading="lazy" src="img/nasty4.jpg" alt=""><div loading="lazy" class="haceFlex"><img loading="lazy" src="img/nasty51.jpg" alt=""><img loading="lazy" src="img/nasty52.jpg" alt=""></div><img loading="lazy" src="img/nasty6.jpg" alt=""><img loading="lazy" src="img/nasty7.jpg" alt=""><img  loading="lazy" src="img/nasty8.jpg" alt=""><div id="contacModal"><img loading="lazy" id="loge" src="img/Logo.jpg" alt=""><div><p>BE CURIOUS</p><p>BE BRAVE</p><p>IT'S POSSIBLE</p><div class="carGa2"><a class="cargaMas2 sis2" href="contact.html">Contact Us</a></div></div></div></div>`;
+        const plantilla=`<div id="fixea">   src="img/nasty1.jpg" alt=""><img  src="img/nasty2.jpg" alt=""><img  src="img/nasty3.jpg" alt=""><img   src="img/nasty4.jpg" alt=""><div class="haceFlex"><img  src="img/nasty51.jpg" alt=""><img src="img/nasty52.jpg" alt=""></div><img  src="img/nasty6.jpg" alt=""><img  src="img/nasty7.jpg" alt=""><img  src="img/nasty8.jpg" alt=""><div id="contacModal"><img  id="loge" src="img/Logo.jpg" alt=""><div><p>BE CURIOUS</p><p>BE BRAVE</p><p>IT'S POSSIBLE</p><div class="carGa2"><a class="cargaMas2 sis2" href="contact.html">Contact Us</a></div></div></div></div>`;
         plantillaa.innerHTML=plantilla;
         let main=document.querySelector("main");
    
