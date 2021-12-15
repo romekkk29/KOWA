446371caf18946784aba1757abcc4f41d3f10006
romekkk29 <romegomez29@gmail.com>
2021-12-14 23:52:19 -0300

    probando

--- js/main.js
+++ js/main.js
@@ -433,7 +433,7 @@ menu.addEventListener("click",function(e){
 xBar.addEventListener("click",function(){
     
     menuBar.style.transform="translate(100%)";
-    body.style.overflowY="auto";
+    
     body.style.WebkitOverflowScrolling="touch"; 
 
 }
